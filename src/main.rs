30d93912a523937fcee994bb548261ed26780196
Andrew Rondeau <git@andrewrondeau.com>
2020-11-04 15:15:54 -0500

    Abandoning attempt to use a strange http server library

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate futures;
-extern crate tide;
+extern crate async_h1;
 
 //use futures::future::poll_fn;
 //use futures::task::Poll;
@@ -7,10 +7,14 @@ use std::collections::VecDeque;
 use std::env;
 use async_std::fs::File;
 use async_std::io::BufReader;
+use async_std::net::{IpAddr, Ipv4Addr, TcpStream, TcpListener, SocketAddr};
 //use async_std::io::BufRead;
 use async_std::prelude::*;
-use std::net::{IpAddr, Ipv4Addr, TcpListener, SocketAddr};
-use tide::{Redirect, Request};
+use async_std::task;
+use http_types::{Response, StatusCode};
+
+//use std::net::{IpAddr, Ipv4Addr, TcpListener, SocketAddr};
+//use tide::{Redirect, Request};
 //use tide::prelude::*;
 //use webbrowser;
 
@@ -18,7 +22,7 @@ static mut static_url_queue: Option<VecDeque<String>> = None;
 static mut static_server_url: Option<String> = None;
 
 #[async_std::main]
-async fn main()  -> tide::Result<()> {
+async fn main()  -> http_types::Result<()> {
 
 	let args: Vec<_> = env::args().collect();
     if args.len() != 2 {
@@ -27,13 +31,21 @@ async fn main()  -> tide::Result<()> {
 
 	let mut url_queue: VecDeque<String> = VecDeque::new();
 
-    let ref filename = args[1];
+	let ref filename = args[1];
+	
+	let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 0);
+	let listener = TcpListener::bind(address).await?;
+	let mut incoming = listener.incoming();
+
+	let server_url = format!("http://{}", listener.local_addr()?);
+
+	println!("Server is running at: {}", server_url);
 
 	let file = File::open(filename).await?;
 	let file_reader = BufReader::new(file);
 	let mut lines = file_reader.lines();
 
-	while let Some(line) = lines.next().await {
+	'lines_loop: while let Some(line) = lines.next().await {
         let l = match line {
         	Ok(l) => l,
         	Err(err) => {
@@ -44,77 +56,28 @@ async fn main()  -> tide::Result<()> {
 		let url = l.trim();
 		let url = format!("{}", url);
 
-		if url.len() > 0 {
-			url_queue.push_back(url);
-		}
-	}
-
-	if url_queue.len() > 0 {
-
-		let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 0);
-		let listener = TcpListener::bind(address)?;
-
-		let mut app = tide::new();
-		app.at("/").get(serve);
-
-		let local_addr = listener.local_addr().unwrap();
-		let task = app.listen(listener);
-			
-		let server_url = format!(
-			"http://{}:{}",
-			local_addr.ip(),
-			local_addr.port());
-
-		println!("Server is running at: {}", server_url);
-
-		unsafe {
-			static_url_queue = Some(url_queue);
-			static_server_url = Some(server_url);
+		if url.len() < 1 {
+    		continue 'lines_loop;
 		}
 
-		open_url();
+		println!("Opening: {}", url); 
+    	webbrowser::open(&server_url).expect("could not open url");
 
-		// Need a way to stop the server
-
-		task.await?;
-	}
-    Ok(())
-}
-
-async fn serve(_req: Request<()>) -> tide::Result {
-
-	unsafe {
-		let mut url_queue = static_url_queue.take().unwrap();
-
-		let url = url_queue.pop_front().unwrap();
-
-		let more_urls = url_queue.len() > 0;
-
-		static_url_queue = Some(url_queue);
-
-		if more_urls {
-			open_url();
-		}
-
-		Ok(Redirect::new(url).into())
-	}
-}
-
-fn open_url() {
-	unsafe {
-		let url_queue = static_url_queue.take().unwrap();
-
-		match &static_server_url {
-			Some(server_url) => {
-				let url = url_queue.get(0).unwrap();
-				println!("Opening: {}", url);
-				webbrowser::open(&server_url).expect("could not open url");
+		match incoming.next().await {
+			Some(stream) => {
+				let stream = stream?;
+				async_h1::accept(stream, |_req| async move {
+					let mut res = Response::new(StatusCode::Ok);
+					res.insert_header("Content-Type", "text/plain");
+					res.set_body("Hello");
+					Ok(res)
+				}).await?;
 			},
 			None => {}
 		}
-
-		static_url_queue = Some(url_queue);
 	}
+	
+    Ok(())
 }
 
 /*
